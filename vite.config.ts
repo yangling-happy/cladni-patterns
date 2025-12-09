30b7edfa379e90c350aa5e815bc746a2db6ee72e
Amber Yang <yamber0302@qq.com>
2025-12-09 16:07:04 +0800

    fix: json配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // 必须是「仓库名/」，末尾的 / 不能少！
-  base: "/cladni-patterns/", 
+  base: "/cladni-patterns/", // 和 homepage 保持一致
   build: {
     outDir: "dist",
-    emptyOutDir: true, // 构建前清空 dist，避免旧文件干扰
-    rollupOptions: {
-      output: {
-        // 强制生成固定命名（可选，方便排查）
-        entryFileNames: `assets/index.js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
+    emptyOutDir: true,
   },
 });
