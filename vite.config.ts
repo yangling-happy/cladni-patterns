b3beb32d5b1f11eb12c5adb18c945b0fc9acec31
Amber Yang <yamber0302@qq.com>
2025-12-26 14:59:04 +0800

    fix: 修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cladni-patterns/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  base: "./",
 });
